950829c711c564e33e14a9d7e14f924153224b46
Matthew Wideman <themotka1337@gmail.com>
2024-09-07 20:13:09 +0300

    init commit

---
+++ cmd/sso/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"jwt-sso/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	logger := initLogger(cfg.Environment)
+	logger.Info("starting application",
+		slog.Any("cfg", cfg))
+
+}
+
+func initLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+	switch env {
+	case envLocal:
+		logger = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return logger
+}
